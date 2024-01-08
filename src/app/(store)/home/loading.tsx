8db62b5558837b13762b3497ff389651de58d986
Danrley <danrley.dev@gmail.com>
2024-01-08 09:55:24 -0300

    feat: Start Project

---
+++ src/app/(store)/home/loading.tsx
@@ -0,0 +1,15 @@
+import { Skeleton } from "@/components/skeleton";
+import Image from "next/image";
+import Load from '../../../../public/load.svg'
+
+
+export default function HomeLoading() {
+  return (
+    <div className="h-[560px] flex justify-center items-center">
+      <div>
+        <Image src={Load} alt="Loading" width={80} />
+        <p>Carregando...</p>
+      </div>
+    </div>
+  )
+}
